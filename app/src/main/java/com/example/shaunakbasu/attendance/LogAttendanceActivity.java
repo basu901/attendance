fb44b15055bffcad2480c0743245bb55faba3ec1
basu901 <basu901@gmail.com>
2017-04-09 15:21:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/shaunakbasu/attendance/LogAttendanceActivity.java
@@ -0,0 +1,13 @@
+package com.example.shaunakbasu.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogAttendanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_attendance);
+    }
+}
